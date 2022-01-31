824f2fca8268fa8dae24f31fbcacfb03e47b219e
cristopher <csmv_8@hotmail.com>
2022-01-31 13:14:25 -0500

    feat(component): add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { ComponentModule } from './component/component.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, ComponentModule],
   controllers: [],
   providers: [],
 })

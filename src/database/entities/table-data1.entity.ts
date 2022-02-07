37e53030e9c65d0a7091943603a2dfb19892af43
cristopher <csmv_8@hotmail.com>
2022-02-07 18:55:41 -0500

    chore(table1): add entity transformer

--- src/database/entities/table-data1.entity.ts
+++ src/database/entities/table-data1.entity.ts
@@ -1,4 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import * as moment from 'moment';
 
 @Entity()
 export class TableData1 {
@@ -17,6 +18,18 @@ export class TableData1 {
   @Column({
     type: 'datetime',
     nullable: true,
+    transformer: {
+      to: (v) => v,
+      from: (v) => {
+        const day = parseInt(moment(v).format('D'));
+        const date = moment(v)
+          .set('date', day + 1)
+          .format('YYYY-MM-DD');
+        const hms = moment().format('HH:mm:ss');
+
+        return date + ' ' + hms;
+      },
+    },
   })
   T1C4: string;
 }
